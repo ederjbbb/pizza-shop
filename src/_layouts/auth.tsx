07664e23cd60718cf640ae00773a8675964c703d
ederjbbb <ederjb@icloud.com>
2024-04-10 20:01:03 +0100

    header icons

--- src/_layouts/auth.tsx
+++ src/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Pizza } from "lucide-react";
 
 export function AuthLayout () {
     return (
-        <div className="min-h-screen lg:grid grid-cols-2 sm:gird grid-row-2">
+        <div className="min-h-screen lg:grid grid-cols-2 sm:gird grid-row-2 antialiased">
             <div className=" h-full border-r border-foreground1/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
                 <div className="flex items-center gap-2 text-lg font-medium text-foreground">
                     <Pizza className="h-5 w-5 " />
